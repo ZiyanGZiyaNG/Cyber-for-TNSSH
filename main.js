e1be75925c537a29024fd7f878f64a35ee36f9e7
ZiyanGZiyaNG <chen971023@gmail.com>
2026-02-06 01:49:57 +0800

    JavaScript

---
+++ main.js
@@ -0,0 +1,8 @@
+const textarea = document.getElementById("msg")
+const button = document.getElementById("send")
+const message = document.getElementById("message")
+
+button.addEventListener("click", function()
+{
+    const py = textarea.value
+});
